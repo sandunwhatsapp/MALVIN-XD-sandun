1a9947eb045533d565fc5cc892f53d02c52706c3
Sandun Lasantha <sandun2003lasantha@gmail.com>
2025-08-04 09:27:30 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~NzklzCxJ#mI6L7tyrQlCbOG3IFVOz6EAsmeU34B58rioqZGSD7IM",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "94788550084", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
